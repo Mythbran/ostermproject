1e540d87416103762ff028d8e95bcc126a4f111b
g-Math <43940112+g-Math@users.noreply.github.com>
2018-11-18 19:22:10 -0500

    Update termproj.c

--- termproj.c
+++ termproj.c
@@ -13,12 +13,40 @@ void *producer(void *param);
 void *consumer(void *param);
 
 int main(int argc, char *argv[]){
-	//GET COMMAND LINE ARGUMENTS
-	//initialize buffer 
-	//create producer thread(s)
-	//create consumer thread(s)
-	//sleep 
-	//EXIT
+	int sleep_time, producer_arg, consumer_arg, i;
+	/*error checking to make sure only 3 command line inputs are provided*/
+	if (argc != 4){
+    	printf("3 inputs needed, program will exit, try again");
+    	exit(1);
+	}
+
+	/* 1. Get command line arguments
+	argv[1],argv[2],argv[3] */
+	sleep_time = strtol(argv[1], NULL, 0);
+	producer_arg = strtol(argv[2], NULL, 0);
+	consumer_arg = strtol(argv[3], NULL, 0);
+	/* 2. Initialize buffer */
+	srand(time(NULL));
+  	pthread_mutex_init(&mutex, NULL);
+  	sem_init(&empty, 0, BUFFER_SIZE); 
+	sem_init(&full, 0, 0);
+
+	/* 3. Create producer thread(s) */
+  	pthread_t producer_x[producer_arg];
+	for(i = 0; i < producer_arg; i++)
+		pthread_create(&producer_x[i], NULL, producer, NULL);
+	/* 4. Create consumer thread(s) */
+		
+		pthread_t consumer_x[consumer_arg];
+  	for(i = 0; i < consumer_arg; i++)
+		pthread_create(&consumer_x[i], NULL, consumer, NULL);
+	
+
+	/* 5. Sleep */
+	sleep(sleep_time);
+	/* 6. Exit */
+	return 0;
+	exit(1);
 }
 
 
